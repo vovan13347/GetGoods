b97afba4d5c629c50996ebb91c5b66cd0657fd02
vovan13347 <vova-lebedev-97@mail.ru>
2024-12-12 12:15:59 +0000

    work ver

--- getgoods.py
+++ getgoods.py
@@ -9,13 +9,8 @@ config.read('config.ini')
 USERS_URL = config.get('API', 'URL')
 
 response = requests.get(USERS_URL)
-names = []
-prices = []
 if response.status_code == 200:
     goods = response.json()
-    for good in goods:
-        names.append(good['name'])
-        prices.append(good['price'])
 else:
     print(f"Failed to fetch data: {response.status_code}")
 
@@ -26,7 +21,7 @@ getgoods = Flask(__name__)
 @getgoods.route('/index')
 def index():
     #user = {'username': 'Vova'}
-    return render_template('index.html',  len = len(names), names=names, prices=prices)
+    return render_template('index.html',  goods=goods)
 
 
 if __name__ == '__main__':
